cf44cad9d4855a2f21d47a1f3134339ecbf1f023
RadoBalkanski <155663399+RadoBalkanski@users.noreply.github.com>
2024-03-08 00:21:23 +0200

    new things

---
+++ script.js
@@ -0,0 +1,48 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyCZmxo6qBWrpRPzDatTRY04bfKp8OGb3eA",
+    authDomain: "bglens-8cfc7.firebaseapp.com",
+    databaseURL: "https://bglens-8cfc7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bglens-8cfc7",
+    storageBucket: "bglens-8cfc7.appspot.com",
+    messagingSenderId: "441776601954",
+    appId: "1:441776601954:web:33dea3f5247ebf7ca7ca3b",
+    measurementId: "G-YMGHHYPT9M"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+
+
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener("click", function (event){
+  event.preventDefault()
+
+  const username = document.getElementById('username').value;
+ 
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    sessionStorage.setItem('username', username);
+    window.location.href = "../ProfilePage/profileIndex.html";
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage); // Display the error message
+});
+
+})
+
