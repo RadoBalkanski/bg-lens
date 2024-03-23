5d5831c990ce5fb753ea6e8d5368bf8b90909e45
RadoBalkanski <155663399+RadoBalkanski@users.noreply.github.com>
2024-03-23 12:06:57 +0200

    needs fixing

--- login.js
+++ login.js
@@ -1,50 +1,42 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyCZmxo6qBWrpRPzDatTRY04bfKp8OGb3eA",
   authDomain: "bglens-8cfc7.firebaseapp.com",
-  databaseURL: "https://bglens-8cfc7-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://bglens-8cfc7-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "bglens-8cfc7",
   storageBucket: "bglens-8cfc7.appspot.com",
   messagingSenderId: "441776601954",
   appId: "1:441776601954:web:33dea3f5247ebf7ca7ca3b",
-  measurementId: "G-YMGHHYPT9M"
+  measurementId: "G-YMGHHYPT9M",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const storage = getStorage(app);
 
-const submit = document.getElementById('submit');
+const submit = document.getElementById("submit");
 
-submit.addEventListener("click", function (event){
-event.preventDefault()
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
 
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-signInWithEmailAndPassword(auth, email, password)
-.then((userCredential) => {
-  // Signed up 
-  const user = userCredential.user;
-  // ...
-})
-.catch((error) => {
-  const errorCode = error.code;
-  const errorMessage = error.message;
-  alert(errorMessage); // Display the error message
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      window.location.href = "../ProfilePage/profileIndex.html";
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage); // Display the error message
+    });
 });
-getDownloadURL(ref(storage, email))
-  .then((url) => {
-    const img = document.getElementById('myimg');
-    img.setAttribute('src', url);
-  })
-  .catch((error) => {
-    console.log(error)
-  });
-
-})
-
--- script.js
+++ script.js
@@ -1,33 +1,39 @@
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCZmxo6qBWrpRPzDatTRY04bfKp8OGb3eA",
+  authDomain: "bglens-8cfc7.firebaseapp.com",
+  databaseURL:
+    "https://bglens-8cfc7-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bglens-8cfc7",
+  storageBucket: "bglens-8cfc7.appspot.com",
+  messagingSenderId: "441776601954",
+  appId: "1:441776601954:web:33dea3f5247ebf7ca7ca3b",
+  measurementId: "G-YMGHHYPT9M",
+};
 
-  import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-  const firebaseConfig = {
-    apiKey: "AIzaSyCZmxo6qBWrpRPzDatTRY04bfKp8OGb3eA",
-    authDomain: "bglens-8cfc7.firebaseapp.com",
-    databaseURL: "https://bglens-8cfc7-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bglens-8cfc7",
-    storageBucket: "bglens-8cfc7.appspot.com",
-    messagingSenderId: "441776601954",
-    appId: "1:441776601954:web:33dea3f5247ebf7ca7ca3b",
-    measurementId: "G-YMGHHYPT9M"
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const storage = getStorage(app);
-
-
-
-const submit = document.getElementById('submit');
+const submit = document.getElementById("submit");
 
 submit.addEventListener("click", function (event) {
   event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
@@ -35,8 +41,7 @@ submit.addEventListener("click", function (event) {
       console.log("User registered");
       console.log(user);
 
-
-      sessionStorage.setItem('username', username);
+      sessionStorage.setItem("username", username);
       // Once the user is registered, proceed with file upload
       const file = document.getElementById("image").files[0];
       if (file) {
@@ -44,23 +49,22 @@ submit.addEventListener("click", function (event) {
         // Upload the file to storage
         return uploadBytes(storageRef, file);
       } else {
-        throw new Error('No file selected');
+        throw new Error("No file selected");
       }
     })
     .then((snapshot) => {
-      console.log('Uploaded a blob or file!');
+      console.log("Uploaded a blob or file!");
 
       // Get the download URL for the uploaded file
       return getDownloadURL(snapshot.ref);
     })
     .then((url) => {
       // Set the image src to the download URL
-      const img = document.getElementById('myimg');
-      img.setAttribute('src', url);
-      console.log('Download URL:', url);
+      const img = document.getElementById("myimg");
+      img.setAttribute("src", url);
+      console.log("Download URL:", url);
 
-
-      sessionStorage.setItem('profileImageUrl', url);
+      sessionStorage.setItem("profileImageUrl", url);
       // Redirect to the next page after successful registration and upload
       // Replace "nextPage.html" with the URL of the next page you want to navigate to
       window.location.href = "../ProfilePage/profileIndex.html";
@@ -70,4 +74,3 @@ submit.addEventListener("click", function (event) {
       alert(error.message);
     });
 });
-
